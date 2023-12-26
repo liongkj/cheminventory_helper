ea2b2bd1eec909693facb5c30aec980eecb662fb
liongkj <20205301+liongkj@users.noreply.github.com>
2023-12-26 17:18:48 +0800

    cosmestic fixes

--- source/background.js
+++ source/background.js
@@ -1,20 +1,26 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: "ON",
-    });
-});
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     if (changeInfo.url) {
-//         console.log(changeInfo.url)
-//         const url = new URL(changeInfo.url);
-//         if (url.hostname === 'app.cheminventory.net') {
-//             chrome.action.setBadgeText({ text: 'ON', tabId: tabId });
-//         } else {
-//             chrome.action.setBadgeText({ text: '', tabId: tabId });
-//         }
-//     }
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.action.setBadgeText({
+//         text: "ON",
+//     });
 // });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url) {
+        const url = new URL(tab.url);
+        if (url.hostname === 'app.cheminventory.net') {
+            chrome.action.setBadgeText({ text: 'ON', tabId: tabId });
+            chrome.action.setBadgeBackgroundColor({ color: [0, 0, 0, 0], tabId: tabId }); // Set color to black
+
+            chrome.action.setIcon({ path: 'logo.png', tabId: tabId }); // Set to disabled icon
+        } else {
+
+            chrome.action.setBadgeBackgroundColor({ color: [128, 128, 128, 0], tabId: tabId }); // Set color to gray
+            chrome.action.setBadgeText({ text: '', tabId: tabId });
+
+            chrome.action.setIcon({ path: 'logo_disabled.png', tabId: tabId }); // Set to disabled icon
+        }
+    }
+});
 
 // Function to make API call
 function makeApiCall(token) {
